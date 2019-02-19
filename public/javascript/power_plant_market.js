bab3938959a86997a7d09c8941c2366c708e1edf
leelanakka <44019459+leelanakka@users.noreply.github.com>
2019-02-19 12:41:25 +0530

    [#50] leela/chandan - display resource market ----------------
    Changes you have made
    
    * current generation power plants can be selected
    * can select one power plant at a time
    
    Impact of changes
    
    * players can current generation power plants
    * players can't select more than one card
    
    Remarks
    
    *

--- public/javascript/power_plant_market.js
+++ public/javascript/power_plant_market.js
@@ -1,3 +1,5 @@
+const currentMarketCards = {};
+
 const resources = {
   Garbage: '<i class="fas fa-trash-alt"></i>',
   Coal: '<i class="fas fa-cubes"></i>',
@@ -7,6 +9,8 @@ const resources = {
 };
 
 const displayMarket = function() {
+  const map = document.getElementById("map");
+  map.onclick = "";
   fetch("/displayPowerPlantMarket")
     .then(res => res.text())
     .then(res => displayPowerPlantMarket(res));
@@ -31,9 +35,13 @@ const generatePowerPlantMarket = function(powerPlantCards) {
 const generateCurrentMarket = function(powerPlants) {
   const currentMarketDiv = generateDiv("single-market", "currentMarket");
   const currentMarket = Object.keys(powerPlants).slice(0, 4);
-  currentMarket.map(powerPlant =>
-    appendPowerPlantCard(currentMarketDiv, powerPlant, powerPlants[powerPlant])
-  );
+  currentMarket.map(powerPlant => {
+    return arrangeCurrentMarket(
+      currentMarketDiv,
+      powerPlant,
+      powerPlants[powerPlant]
+    );
+  });
   return currentMarketDiv;
 };
 
@@ -41,19 +49,44 @@ const generateFutureMarket = function(powerPlants) {
   const futureMarketDiv = generateDiv("single-market", "futureMarket");
   const futureMarket = Object.keys(powerPlants).slice(4);
   futureMarket.map(powerPlant =>
-    appendPowerPlantCard(futureMarketDiv, powerPlant, powerPlants[powerPlant])
+    arrangeFurtureMarket(futureMarketDiv, powerPlant, powerPlants[powerPlant])
   );
   return futureMarketDiv;
 };
 
-const appendPowerPlantCard = function(singleMarket, powerPlant, cardDetails) {
+const arrangeCurrentMarket = function(singleMarket, powerPlant, cardDetails) {
   const powerPlantCardId = `powerPlant_${powerPlant}`;
-  const powerPlantCardDiv = generateDiv("power-plant-card", powerPlantCardId);
+  const powerPlantCardDiv = generateDiv("unselectedCard", powerPlantCardId);
+  powerPlantCardDiv.onclick = addFocus.bind(null, powerPlantCardDiv);
   const priceDiv = generatePriceDiv(powerPlant);
   const resourceDiv = generateResourceDiv(cardDetails);
   powerPlantCardDiv.appendChild(priceDiv);
   powerPlantCardDiv.appendChild(resourceDiv);
   singleMarket.appendChild(powerPlantCardDiv);
+  currentMarketCards[powerPlant] = {
+    isSelected: false,
+    powerplant: powerPlantCardDiv
+  };
+};
+
+const arrangeFurtureMarket = function(singleMarket, powerPlant, cardDetails) {
+  const powerPlantCardId = `powerPlant_${powerPlant}`;
+  const powerPlantCardDiv = generateDiv("unselectedCard", powerPlantCardId);
+  const priceDiv = generatePriceDiv(powerPlant);
+  const resourceDiv = generateResourceDiv(cardDetails);
+  powerPlantCardDiv.appendChild(priceDiv);
+  powerPlantCardDiv.appendChild(resourceDiv);
+  singleMarket.appendChild(powerPlantCardDiv);
+};
+
+const addFocus = function(element) {
+  Object.keys(currentMarketCards).forEach(card => {
+    currentMarketCards[card].isSelected = false;
+    currentMarketCards[card].powerplant.className = "unselectedCard";
+  });
+  const id = element.id.slice(-1);
+  currentMarketCards[id].isSelected = true;
+  element.className = "selectedCard";
 };
 
 const generateResourceDiv = function(cardDetails) {
