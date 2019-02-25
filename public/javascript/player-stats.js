4bf77212e0234fcc327a40e0a1a54223ff502b68
Gaurav Saini <44020369+sgauravk@users.noreply.github.com>
2019-02-25 18:44:13 +0530

    [#46] Deepika/Gaurav - player stats
    Changes you have made
    
    * implement a feature to update the numbers of cities in player stats
    
    Impact of changes
    
    * now user can see how many city he build in the player stat
    
    Remarks
    
    *

--- public/javascript/player-stats.js
+++ public/javascript/player-stats.js
@@ -1,4 +1,5 @@
-const updatePlayerStatsDiv = function({ name, resources, powerplants, money }) {
+const updatePlayerStatsDiv = function(playerStats) {
+  const { name, resources, powerplants, money, cities } = playerStats
   let count = 1;
   document.getElementById("player-name").innerText = name;
   ["Coal", "Oil", "Garbage", "Uranium"].forEach(
@@ -8,6 +9,7 @@ const updatePlayerStatsDiv = function({ name, resources, powerplants, money }) {
   const powerplantsCost = Object.keys(powerplants).slice(0, 3);
   powerplantsCost.forEach(addPowerPlantToPlayer.bind(null,count,powerplants));
   document.getElementById("player-money").innerText = money;
+  document.getElementById("player-cities").innerText = cities;
 };
 
 const getPlayerStatsDiv = function() {
@@ -15,5 +17,6 @@ const getPlayerStatsDiv = function() {
     .then(res => res.json())
     .then(playerStats => {
       updatePlayerStatsDiv(playerStats);
+      console.log(playerStats);
     });
 };
