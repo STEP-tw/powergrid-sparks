485cbeac62bbe7c3a7793f9ace4230059d83b7b9
Gaurav Saini <44020369+sgauravk@users.noreply.github.com>
2019-02-27 14:53:06 +0530

    [#35] Keerthy/Gaurav - Return Resources
    Changes you have made
    
    * change UI
    
    Impact of changes
    
    *
    
    Remarks
    
    *

--- public/javascript/resourceMarketTemplate.js
+++ public/javascript/resourceMarketTemplate.js
@@ -7,7 +7,7 @@ const getBiddingSectionTemplate = function() {
     <div class="increase-bid auction">Increase bid : 
     <span id="bid-amount"></span>&nbsp
     <button class='increase-bid-button button' id="increase_bid" onclick = 'increaseBid()'>&#x2b</button></div>
-    <button class = 'bid-option' onclick="makeBid()">Make Bid</button>&nbsp<button class='bid-option' onclick="pass()">Pass</button>
+    <button class = 'auction bid-option' onclick="makeBid()">Make Bid</button>&nbsp<button class='auction bid-option' onclick="pass()">Pass</button>
     <button class="bid-option" onclick = "startBuyingResources()" id="buy-resources-button" style="visibility:hidden" >Buy Resources</button>
     
     <div id="selected-resource-amount" style="visibility:hidden">
